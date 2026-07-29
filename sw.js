11be4880c10ce21ac6a5cf6fc432f6740f133628
Centro de Monitoreo SSSur <centro.monitoreo.sssur@gmail.com>
2026-07-29 15:44:59 -0600

    Implementación de Real Time Supabase

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v1.0.1';
+const CACHE_VERSION = 'v1.0.2';
 const CACHE_NAME = `cm-sssur-cache-${CACHE_VERSION}`;
 
 // Recursos estáticos mínimos requeridos para la app offline
