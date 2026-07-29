db951bd6dbece6d32109ed35bc0c53eacdce36a1
Centro de Monitoreo SSSur <centro.monitoreo.sssur@gmail.com>
2026-07-29 16:06:19 -0600

    version 1.0.3 correcion mapa en vivo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v1.0.2';
+const CACHE_VERSION = 'v1.0.3';
 const CACHE_NAME = `cm-sssur-cache-${CACHE_VERSION}`;
 
 // Recursos estáticos mínimos requeridos para la app offline
